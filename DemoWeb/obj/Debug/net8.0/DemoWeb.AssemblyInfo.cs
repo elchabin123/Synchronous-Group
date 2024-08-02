1cc98c83d1bbd473a33c34b0132494c7669f7b81
BUITHITUYETNGOC <buithituyetngoc2402@gmail.com>
2024-08-02 15:30:49 +0700

    update lại Home

--- DemoWeb/obj/Debug/net8.0/DemoWeb.AssemblyInfo.cs
+++ DemoWeb/obj/Debug/net8.0/DemoWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f0b587dac2251905e915695b82fc065851a5d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9660aac513797d4985861f75a664b4e0051b5e19")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
