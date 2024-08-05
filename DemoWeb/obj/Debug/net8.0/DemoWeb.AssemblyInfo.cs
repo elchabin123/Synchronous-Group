867832f8ca2d92098f3144c7dc3f31a032f1cf25
elchabin123 <nghbvinh19@gmail.com>
2024-08-05 13:57:57 +0700

    fix display cart everywhere and delete the home view

--- DemoWeb/obj/Debug/net8.0/DemoWeb.AssemblyInfo.cs
+++ DemoWeb/obj/Debug/net8.0/DemoWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00ff9b503d03e19d5a32d59787e44ac7bd07bb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4669d7bf1ef09c3ddff5a0af80143f68c0a5ff30")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
