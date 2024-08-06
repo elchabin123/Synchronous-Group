7e6763fa8fec451623909f8aaf0277eb6eaeeb28
elchabin123 <nghbvinh19@gmail.com>
2024-08-06 10:19:42 +0700

    fix some checkSpell

--- DemoWeb/obj/Debug/net8.0/DemoWeb.AssemblyInfo.cs
+++ DemoWeb/obj/Debug/net8.0/DemoWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4669d7bf1ef09c3ddff5a0af80143f68c0a5ff30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c37822ed14ef7f1d89ff1ca51f35c88bf30d79dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
